f0d4c2785a102c66873c5c55342e9f0d284a911d
Matthew Borden <borden0108@gmail.com>
2013-02-24 19:14:51 -0800

    Updated app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
  * Module dependencies.
  */
 
-var express  = require('express')
-  , routes   = require('./routes')
-  , user     = require('./routes/user')
-  , http     = require('http')
-  , path     = require('path'),
+var express  = require('express'),
+  routes   = require('./routes'),
+  user     = require('./routes/user'),
+  http     = require('http'),
+  path     = require('path'),
   couchdb    = require('felix-couchdb'),
   client     = couchdb.createClient(5984, 'localhost'),
   crypto     = require('crypto'),
